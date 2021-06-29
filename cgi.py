855cdf3a717382a0078cd27a399862424d672b01
cdar-cyber <73689475+cdar-cyber@users.noreply.github.com>
2021-06-29 16:32:28 +0530

    Add files via upload

---
+++ cgi.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import cgi
+import subprocess
+
+print("content-type: text/html")
+print()
+
+f = cgi.FieldStorage()
+cmd = f.getvalue("x")
+
+#print(cmd)
+o = subprocess.getoutput("sudo "+ cmd)
+print(o)
